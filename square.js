e5f0de0ce09548fb586d88f3fef07ddb15d0d3d2
Bowie Wan <bowiewan@gmail.com>
2022-06-26 16:11:41 -0500

    func return square of argument

---
+++ square.js
@@ -0,0 +1,22 @@
+//Now you have to write a function that takes an argument and returns the square of it.
+
+var square = function(a){
+    return a*a;
+}
+
+const square = n => n * n;
+const square = n => Math.pow(n, 2);
+
+function square(x){
+    return x ** 2;
+}
+
+
+function square(num){
+    var num = Math.pow(num, 2);
+    return num;
+}
+
+const square = x => {
+    return Math.sqrt(x**4);
+}
