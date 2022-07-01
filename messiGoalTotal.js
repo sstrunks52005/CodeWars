bdae1eaec774a1ece942d85c485c58d49d8b4c24
Bowie Wan <bowiewan@gmail.com>
2022-07-01 05:05:05 -0500

    messi goals adding messi goal

---
+++ messiGoalTotal.js
@@ -0,0 +1,6 @@
+//Use variables to find the sum of the goals Messi scored in 3 competitions
+
+var laLigaGoals =43
+var championsLeagueGoals = 10
+var copaDelReyGoals = 5
+var total = laLigaGoals + championsLeagueGoals + copaDelReyGoals
