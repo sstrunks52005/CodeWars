7e15515adb5656f0931eb89e6b4e2c4df98538dc
Bowie Wan <bowiewan@gmail.com>
2022-07-07 05:05:05 -0500

    add sayHello add sayHello.js

---
+++ sayHello.js
@@ -0,0 +1,15 @@
+// The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+
+// Example output: Hello, Mr. Spock
+
+function sayHello(name) {
+    return `Hello, ${name}`
+}
+
+  const sayHello = name => `Hello, ${name}`
+
+  function sayHello(name) {
+    return 'Hello, ' + name;
+  }
+
+const sayHello = name => `Hello, ${name}`
